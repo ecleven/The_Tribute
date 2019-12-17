a8871759496f2f83acc61a639cc8305c37e0e9e5
Edward Cleven <eddie@Edwards-MacBook-Pro.local>
2019-12-16 19:50:34 -0700

    updating background

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import os
-
-
-cwd = os.getcwd()
-print(cwd)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return render_template('%s.html' % 'main')
+    image_names = os.listdir('./images')
+    print(image_names)
+    return render_template("main.html", image_names=image_names)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
