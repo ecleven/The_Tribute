39e54e424a07b5a9631f5a79b44322040367b0c8
Edward Cleven <edwardcleven@Edwards-iMac.local>
2020-01-02 15:04:08 -0700

    added style

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, session, url_for, flash, redirect
+from flask_bcrypt import Bcrypt
+ 
+from forms import RegistrationForm, LoginForm
 import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.urandom(16)
+
+#app.secret_key = print(os.urandom(16))
 
 da_bros = [
      { "name": "Austin", "image": "austin.JPG", "bio": "Media mogul Austin Sabella CPA is the brains and the chin behind the bros. Born in 1993 in the slums of algonquin, austin discovered his love for wrestling playing tummy sticks with his cousin Corey. It quickly became apperent that he was a natural grapler, and Austin through the ranks of IL wresting until a self inflicted injury stopped his career short. Since then Austin has rebranded himself as a prestigous businessman becoming CFO of a media consultancy/Indian staffing firm." },
@@ -12,15 +19,40 @@ da_bros = [
      { "name": "Joey", "image": "joey.jpg", "bio": "The muscle of the group, Joey has a reputation for kicking ass and taking names. At a young age Joey crossed paths with the great and wonderful Tony Ramos, emboding him with the spirit of the warrior. He has since put that power to great use, being one of the most feared men in Carbonale after single handely splading an entire rival fraternaty. Joey has since calmed down, but legend has it every Eve's Eve he will glow with the power of Ramos" },
      { "name": "Rob", "image": "rob.jpeg", "bio": "Rob is a celebrated seaman, sexual maverick, alleged toilet clogger, and pathological liar. Rob had a brief stint at the highschool of Marian Central before leaving after discovering that many of the girls had taken a vow of chastity. He has since gained favor again with the bros, joing the group in 2018 after the group voted 4-3 to let him in. Rob is now a valued member of the group, always down to lend a hand, or tell you about his most recent lay." },
 ]          
-    
+
+
+#login 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/')
-def main_page():
+def home():
     return render_template("main.html", da_bros=da_bros)
 
 @app.route('/upload/<filename>')
 def send_image(filename):
     return send_from_directory("images/Da-Bros", filename)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
